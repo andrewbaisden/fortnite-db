440117108cf3b69d8e2b8ef544f545e88a9db516
Andrew Baisden <andrewbaisden@gmail.com>
2018-12-31 16:55:32 +0000

    added rest api

---
+++ backend-rest/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const itemController = require('./controllers/fortnite');
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.get('/items', (req, res) => {
+    const data = require('./data/fortniteData.json');
+    res.json(data);
+})
+
+app.get('/items/weapons/:itemId', itemController.getItemWeapon);
+app.get('/items/outfits/:itemId', itemController.getItemOutfit);
+app.get('/items/gliders/:itemId', itemController.getItemGlider);
+app.get('/items/backblings/:itemId', itemController.getItemBackBling);
+app.get('/items/loadingscreens/:itemId', itemController.getItemLoadingScreen);
+app.get('/items/spraydecals/:itemId', itemController.getItemSprayDecal);
+app.get('/items/emotes/:itemId', itemController.getItemEmote);
+app.get('/items/pickaxes/:itemId', itemController.getItemPickaxes);
+app.get('/items/contrails/:itemId', itemController.getItemContrail);
+app.get('/items/toys/:itemId', itemController.getItemToys);
+
+app.use((req, res, next) => {
+    res.status(404).send('404 Page Not Found')
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
