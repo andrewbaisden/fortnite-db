d779d8fcf9c908175eabcbedb8ba7cadb547293c
Andrew Baisden <andrewbaisden@gmail.com>
2019-01-01 14:40:48 +0000

    graphQL API filtered queries for weapons added

--- backend-graphql/src/resolvers/Query.js
+++ backend-graphql/src/resolvers/Query.js
@@ -2,14 +2,47 @@ const axios = require('axios');
 
 const Query = {
 
-        // Fortnite API
-        fortniteweapons: async (parent, args, info) => {
+        // Fortnite API - GET weapons and filter by name
+        fortniteweaponsfiltername: async (parent, args, info) => {
             const { data } = await axios.get('http://localhost:5000/items/weapon')
             if (!args.query) {
               return data  
             }
             return data.filter((weapon) => {
               return weapon.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+          },
+
+        // Fortnite API - GET weapons and filter by type
+        fortniteweaponsfiltertype: async (parent, args, info) => {
+            const { data } = await axios.get('http://localhost:5000/items/weapon')
+            if (!args.query) {
+              return data  
+            }
+            return data.filter((weapon) => {
+              return weapon.type.toLowerCase().includes(args.query.toLowerCase())
+            })
+          },
+
+        // Fortnite API - GET weapons and filter by weapon type
+        fortniteweaponsfilterweapontype: async (parent, args, info) => {
+            const { data } = await axios.get('http://localhost:5000/items/weapon')
+            if (!args.query) {
+              return data  
+            }
+            return data.filter((weapon) => {
+              return weapon.weaponType.toLowerCase().includes(args.query.toLowerCase())
+            })
+          },
+
+        // Fortnite API - GET weapons and filter by weapon item rarity
+        fortniteweaponsfilterweaponitemrarity: async (parent, args, info) => {
+            const { data } = await axios.get('http://localhost:5000/items/weapon')
+            if (!args.query) {
+              return data  
+            }
+            return data.filter((weapon) => {
+              return weapon.itemRarity.toLowerCase().includes(args.query.toLowerCase())
             })
           },
 }
