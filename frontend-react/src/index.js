6a7cbaca7ade7740eb78794c0513b5e869b9b862
Andrew Baisden <andrewbaisden@gmail.com>
2019-01-01 15:44:06 +0000

    frontend react app created

---
+++ frontend-react/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+export const client = new ApolloClient({
+    uri: "http://localhost:8000/"
+});
+
+client.query({
+    query: gql`
+        query {
+            fortniteweaponsfilterweaponitemrarity(query: "Legendary") {
+            id
+            name
+            information {
+            damage
+            }
+         }
+    }
+    `
+})
+.then(result => console.log(result));
+
+ReactDOM.render(<App />, document.querySelector('#root'));
