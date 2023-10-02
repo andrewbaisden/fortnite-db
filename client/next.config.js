d016a1c2e618af40bcc51b806b83e9bb3fb16fec
Andrew <andrewbaisden@gmail.com>
2023-10-02 16:28:54 +0100

    fixed next.config.js file with correct domain for images

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'onrender.com',
-        port: '',
-        pathname: '/static/assets/weapons/**',
-      },
-    ],
+    domains: ['fortnite-db-backend-server.onrender.com'],
   },
 };
